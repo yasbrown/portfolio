3237892e2e549b329cd489ffb66bb0b55e972512
yasmin brown <yasminbrown6@hotmail.com>
2022-12-24 00:00:35 +0000

    header component styling

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,10 +6,18 @@ const Header = () => {
   return (
     <header>
       <div className="container header_container">
-        <h1>Y A S M I N</h1>
-        <h1>B R O W N</h1>
-        <h2>F U L L   S T A C K</h2>   <h2>W E B</h2>   <h2>D E V E L O P E R</h2>
-        <CTA />
+        <div className="header_name">
+          <h1>Y A S M I N</h1>
+          <h1>B R O W N</h1>
+        </div>
+        <div className="header_title">
+          <h2>F U L L   S T A C K</h2>
+          <h2>W E B</h2>
+          <h2>D E V E L O P E R</h2>
+        </div>
+        <div className="header_btn">
+          <CTA />
+        </div>
       </div>
     </header>
   )
