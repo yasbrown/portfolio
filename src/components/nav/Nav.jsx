ce979c98e9ffbcc4cc15a5890b66f822da749a94
yasmin brown <yasminbrown6@hotmail.com>
2023-01-03 12:12:18 +0000

    added js to navbar active class

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './nav.css'
+import {useState} from 'react'
 
 const Nav = () => {
+  const [activeNav, setActiveNav] = useState('');
   return (
     <nav>
-      <a href="#header">Yasmin Brown</a>
+      <a href="#header" onClick={() => setActiveNav('')}>Yasmin Brown</a>
       <div className="nav-btns">
-        <a href="#about" className='active'>About</a>
-        <a href="#experience">Projects</a>
-        <a href="#contact">Contact</a>
+        <a href="#about" onClick={() => setActiveNav('#about')} className={ activeNav === '#about' ? 'active' : '' }>About</a>
+        <a href="#experience" onClick={() => setActiveNav('#experience')} className={ activeNav === '#experience' ? 'active' : '' }>Projects</a>
+        <a href="#contact" onClick={() => setActiveNav('#contact')} className={ activeNav === '#contact' ? 'active' : '' }>Contact</a>
       </div>
     </nav>
   )
