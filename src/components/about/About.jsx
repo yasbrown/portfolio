046614e6f8f6405b91cd78be45ecd25f54a2d994
yasmin brown <yasminbrown6@hotmail.com>
2023-01-16 17:12:43 +0000

    added about me text and changed font colour

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -3,7 +3,17 @@ import './about.css'
 
 const About = () => {
   return (
-    <section id="about">About</section >
+    <section id="about">
+      <h2>About me</h2>
+
+      <h2>Hi, my name is Yasmin.</h2>
+      <div className="container about__content">
+        <p>I am a fullstack developer currently based in London, UK. Originally from a successful background in clinical medicine, I have always thrived in agile environments that allow me to find creative solutions to complex problems.</p>
+        <p>Although I have since swapped my stethoscope for a MacBook I have spent my professional career relishing any opportunity to communicate my ideas with others, whether that be as a teaching assistant at Le Wagon, or discussing challenging ideas with clients and stakeholders.</p>
+        <p>It is this diverse skillset which I utilise in order to create impact.</p>
+      </div>
+
+    </section >
   )
 }
 
