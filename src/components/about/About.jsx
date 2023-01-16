22854adf15a0d4bd8173e7c8f54ae1ff74a059a3
yasmin brown <yasminbrown6@hotmail.com>
2023-01-16 17:50:33 +0000

    added stack icons

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,5 +1,13 @@
 import React from 'react'
 import './about.css'
+import RUBY from '../../assets/Ruby.png'
+import RAILS from '../../assets/Rails.png'
+import REACT from '../../assets/React.png'
+import JS from '../../assets/Javascript.png'
+import HTML from '../../assets/HTML5.png'
+import CSS from '../../assets/CSS3.png'
+import POSTGRES from '../../assets/Postgresql.png'
+import HEROKU from '../../assets/Heroku.png'
 
 const About = () => {
   return (
@@ -11,6 +19,18 @@ const About = () => {
         <p>I am a fullstack developer currently based in London, UK. Originally from a successful background in clinical medicine, I have always thrived in agile environments that allow me to find creative solutions to complex problems.</p>
         <p>Although I have since swapped my stethoscope for a MacBook I have spent my professional career relishing any opportunity to communicate my ideas with others, whether that be as a teaching assistant at Le Wagon, or discussing challenging ideas with clients and stakeholders.</p>
         <p>It is this diverse skillset which I utilise in order to create impact.</p>
+      </div>
+
+      <h2>My stack.</h2>
+      <div className="stack__icons">
+      <img src={RUBY} alt="Ruby icon" />
+      <img src={RAILS} alt="Rails icon" />
+      <img src={REACT} alt="React icon" />
+      <img src={JS} alt="Javascript icon" />
+      <img src={HTML} alt="HTML5 icon" />
+      <img src={CSS} alt="CSS3 icon" />
+      <img src={POSTGRES} alt="PostgreSQL icon" />
+      <img src={HEROKU} alt="Heroku icon" />
       </div>
 
     </section >
