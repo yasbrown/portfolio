8cc5007f927935e8c7fe6cebd80f262cba5c5c44
yasmin brown <yasminbrown6@hotmail.com>
2023-01-16 18:03:30 +0000

    bassic CSS styling for about component

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -12,25 +12,31 @@ import HEROKU from '../../assets/Heroku.png'
 const About = () => {
   return (
     <section id="about">
-      <h2>About me</h2>
+      <div className="about__header">
+        <h2>About me</h2>
+      </div>
 
-      <h2>Hi, my name is Yasmin.</h2>
+      <div className="container about__greeting">
+        <h2>Hi, my name is Yasmin.</h2>
+      </div>
       <div className="container about__content">
         <p>I am a fullstack developer currently based in London, UK. Originally from a successful background in clinical medicine, I have always thrived in agile environments that allow me to find creative solutions to complex problems.</p>
         <p>Although I have since swapped my stethoscope for a MacBook I have spent my professional career relishing any opportunity to communicate my ideas with others, whether that be as a teaching assistant at Le Wagon, or discussing challenging ideas with clients and stakeholders.</p>
         <p>It is this diverse skillset which I utilise in order to create impact.</p>
       </div>
 
-      <h2>My stack.</h2>
+      <div className="container stack__header">
+        <h2>My stack</h2>
+      </div>
       <div className="stack__icons">
-      <img src={RUBY} alt="Ruby icon" />
-      <img src={RAILS} alt="Rails icon" />
-      <img src={REACT} alt="React icon" />
-      <img src={JS} alt="Javascript icon" />
-      <img src={HTML} alt="HTML5 icon" />
-      <img src={CSS} alt="CSS3 icon" />
-      <img src={POSTGRES} alt="PostgreSQL icon" />
-      <img src={HEROKU} alt="Heroku icon" />
+        <img src={RUBY} alt="Ruby icon" />
+        <img src={RAILS} alt="Rails icon" />
+        <img src={REACT} alt="React icon" />
+        <img src={JS} alt="Javascript icon" />
+        <img src={HTML} alt="HTML5 icon" />
+        <img src={CSS} alt="CSS3 icon" />
+        <img src={POSTGRES} alt="PostgreSQL icon" />
+        <img src={HEROKU} alt="Heroku icon" />
       </div>
 
     </section >
