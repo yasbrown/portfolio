8328b871caf809ec47e03f78d539298d71aa55dc
yasmin brown <yasminbrown6@hotmail.com>
2023-01-21 16:25:07 +0000

    added projects to portfolio page

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
       <a href="#header" onClick={() => setActiveNav('')}>Yasmin Brown</a>
       <div className="nav-btns">
         <a href="#about" onClick={() => setActiveNav('#about')} className={ activeNav === '#about' ? 'active' : '' }>About</a>
-        <a href="#experience" onClick={() => setActiveNav('#experience')} className={ activeNav === '#experience' ? 'active' : '' }>Projects</a>
+        <a href="#projects" onClick={() => setActiveNav('#experience')} className={ activeNav === '#experience' ? 'active' : '' }>Projects</a>
         <a href="#contact" onClick={() => setActiveNav('#contact')} className={ activeNav === '#contact' ? 'active' : '' }>Contact</a>
       </div>
     </nav>
