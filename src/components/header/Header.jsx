87948b51e6d358a6c27cdbc5f38ad8598fb61635
yasmin brown <yasminbrown6@hotmail.com>
2023-01-30 14:37:22 +0000

    added framer motion to header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,11 +2,12 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import PAINT from '../../assets/Paint-header.png'
+import { motion as m } from "framer-motion"
 
 const Header = () => {
   return (
     <header id="header">
-      <div className="container header_container">
+      <m.div initial={{ opacity: 0 }} animate={{ opacity: 1 }} transition={{ duration: 0.85 }} className="container header_container">
         <div className="header_name">
           <h1>Y A S M I N</h1>
           <h1>B R O W N</h1>
@@ -20,7 +21,7 @@ const Header = () => {
         <div className="header_btn">
           <CTA />
         </div>
-      </div>
+      </m.div>
     </header>
   )
 }
