733c160c8d12c5cb36f56b43f5ae72db3ad858a2
yasmin brown <yasminbrown6@hotmail.com>
2023-02-09 18:19:12 +0000

    added greeting loop to header component

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -3,6 +3,40 @@ import './header.css'
 import { motion as m } from "framer-motion"
 
 const Header = () => {
+  const cycle = () => {
+      const greetings = ["Bonjour", "Konnichiwa", "Hallo", "Ciao", "Guten Tag", "Hola", "Hi"];
+      let counter = 0;
+
+    // while (counter < greetings.length) {
+    //   let hi = document.getElementById("hello")
+    //   hi.innerHTML = greetings[counter];
+    //   counter++;
+    // }
+
+    if (counter < greetings.length) {
+      let hi = document.getElementById("hello")
+      hi.innerHTML = greetings[counter];
+      counter++;
+      console.log(counter)
+    } else {
+      counter = 0;
+    }
+
+    // for (const greeting of greetings){
+    //   console.log(greeting);
+    //   let hi = document.getElementById("hello")
+    //   hi.innerHTML = greeting;
+    // }
+
+    // for (let counter = 0; counter < greetings.length; counter++) {
+    //   console.log(greetings[counter])
+    //   let hi = document.getElementById("hello")
+    //   hi.innerHTML = greetings[counter];
+    // }
+    }
+
+  setInterval(cycle, 2300);
+
   return (
     <header id="header">
       <div className='container header__content'>
@@ -13,7 +47,7 @@ const Header = () => {
           className="header__container">
 
           <div className="header__text_greeting">
-            <h1>Hi,</h1>
+            <h1><span id="hello">Hi</span>,</h1>
           </div>
           <div className="header__text">
             <h1>I'm Yasmin.</h1>
