aaf99cdf106addff2f4dbbd84a116f5680d29353
yasmin brown <yasminbrown6@hotmail.com>
2023-02-18 13:35:53 +0000

    updated framer motion animations for project components to occur on scroll, abstracted out projects into individual components

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,37 +1,34 @@
-import React from "react";
+import React from "react"
 import './projects.css'
-import WALKIN from '../../assets/Mockup Walkin.png'
-import AIR from '../../assets/AirBroomNBroom.png'
-import LOGO from '../../assets/WalkIn Logo.png'
-import {BsGithub} from 'react-icons/bs'
-import { motion as m } from "framer-motion"
+import Project1 from "./Project1"
+import Project2 from "./Project2"
 
 const Projects = () => {
-  const portfolioImageAnimate = {
-    offscreen:{
-      y:100,
-      opacity:0
-    },
-    onscreen:{
-      y:0,
-      opacity:1,
-      transition:{
-        duration: 1.5,
-        type: "spring" }
-    }
-  }
+  // const portfolioImageAnimate = {
+  //   offscreen:{
+  //     y:100,
+  //     opacity:0
+  //   },
+  //   onscreen:{
+  //     y:0,
+  //     opacity:1,
+  //     transition:{
+  //       duration: 1.5,
+  //       type: "spring" }
+  //   }
+  // }
 
-  const portfolioTextAnimate = {
-    offscreen:{
-      x:-100,
-      opacity:0 },
-    onscreen:{
-      x:0,
-      opacity:1,
-      transition:{
-        duration: 1.5,
-        type: "spring" }}
-  }
+  // const portfolioTextAnimate = {
+  //   offscreen:{
+  //     x:-100,
+  //     opacity:0 },
+  //   onscreen:{
+  //     x:0,
+  //     opacity:1,
+  //     transition:{
+  //       duration: 1.5,
+  //       type: "spring" }}
+  // }
 
   return (
     <section id="projects">
@@ -41,67 +38,8 @@ const Projects = () => {
 
       <div className="portfolio__content">
         <div className="portfolio__projects">
-
-          <m.div className='walkin'
-            initial={ "offscreen" }
-            whileInView={ "onscreen" }
-            viewport={{ once:false, amount:0.5 }}
-            transition={{ staggerChildren:0.8 }}
-            >
-            <m.img
-              variants={ portfolioImageAnimate }
-              src={WALKIN} alt="WalkIn Mockup"
-              />
-            <m.div className='project__info'
-              variants={ portfolioTextAnimate }
-              >
-              <div className='project__info__name__type'>
-                <h2>WalkIn</h2>
-                <p>Mobile</p>
-              </div>
-              <div className='project__description__stack'>
-                <p>A web app designed to answer the question: “Where can I get a reservation tonight?”</p>
-                <p>Ruby on Rails  |  JavaScript  |  HTML 5  |  CSS |  PostgreSQL  |  Heroku</p>
-              </div>
-              <div className='project__links'>
-                <a href="https://walk--in.herokuapp.com/" target="_blank" rel='noopener noreferrer'>
-                  <img src={LOGO} alt="Link to WalkIn webpage"/>
-                </a>
-                <a href="https://github.com/yasbrown/walk-in" target="_blank" rel='noopener noreferrer'>
-                  <BsGithub />
-                </a>
-              </div>
-            </m.div>
-          </m.div>
-
-          <m.div className='airbroomnbroom'
-            initial={ "offscreen" }
-            whileInView={ "onscreen" }
-            viewport={{ once:false, amount:0.5 }}
-            transition={{ staggerChildren:0.5 }}
-            >
-            <m.img src={AIR} alt="AirbroomNBroom Mockup"
-              variants={ portfolioImageAnimate }
-              />
-            <m.div className='project__info'
-              variants={ portfolioTextAnimate }
-              >
-              <div className='project__info__name__type'>
-                <h2>AirBroomNBroom</h2>
-                <p>Desktop</p>
-              </div>
-              <div className='project__description__stack'>
-                <p>An online marketplace to rent broomsticks (AirBnB clone).</p>
-                <p>Ruby on Rails  |  JavaScript  |  HTML 5  |  CSS |  PostgreSQL  |  Heroku</p>
-              </div>
-              <div className='project__links'>
-                <a href="https://github.com/yasbrown/walk-in" target="_blank" rel='noopener noreferrer'>
-                  <BsGithub />
-                </a>
-              </div>
-            </m.div>
-          </m.div>
-
+          <div className='walkin'><Project1 /></div>
+          <div className='walkin'><Project2 /></div>
         </div>
       </div>
 
