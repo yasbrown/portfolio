09250395adab5d928cfe446b2b07c83dac423f5f
yasmin brown <yasminbrown6@hotmail.com>
2023-02-19 21:51:02 +0000

    added footer component + css styling

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './footer.css'
+
+const Footer = () => {
+  return (
+    <div>
+      <p className='footer__text'>Made with React</p>
+    </div>
+  )
+}
+
+export default Footer
